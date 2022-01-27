0dffc59515f5e20cfcc62b1ce54840e2932f5a33
aryawdy <aryawdy16@gmail.com>
2022-01-27 08:45:22 +0700

    login sm register

--- app.js
+++ app.js
@@ -3,13 +3,19 @@ const express = require('express')
 const app = express()
 const port = 3000
 const Controller = require('./controllers/controller')
+const UserController = require('./controllers/userController')
 
 app.set('view engine', 'ejs')
 app.use(express.urlencoded({extended: false}))
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+
+
+app.get('/', UserController.getLogin)
+app.post('/', UserController.postLogin)
+app.get('/register', UserController.getRegister)
+app.post('/register', UserController.postRegister)
+
+
 app.get('/service', Controller.showService)
 app.get('/service/:id/', Controller.getBooking)
 app.post('/service/:id/', Controller.postBooking)
