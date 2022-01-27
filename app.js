d6948bf25f90fe4df81534533a68a7a7f1855e81
narinda giriputra <narinda.giriputra@gmail.com>
2022-01-28 07:21:38 +0800

    heroku

--- app.js
+++ app.js
@@ -1,36 +1,8 @@
-
-
-// const express = require('express')
-// const app = express()
-// const port = 3000
-// const Controller = require('./controllers/controller')
-// const UserController = require('./controllers/userController')
-// const session = require('express-session')
-
-// app.set('view engine', 'ejs')
-// app.use(express.urlencoded({extended: false}))
-
-
-
-// app.get('/', UserController.getLogin)
-// app.post('/', UserController.postLogin)
-// app.get('/register', UserController.getRegister)
-// app.post('/register', UserController.postRegister)
-
-
-// app.get('/service', Controller.showService)
-// app.get('/service/:id/', Controller.getBooking)
-// app.post('/service/:id/', Controller.postBooking)
-// app.get('/user/:id/service', (req, res) => {
-//   res.send('Hello World!')
-// })
-
-
-
 const express = require('express')
 const route = require('./routes')
 const app = express()
-const port = 3000
+// const port = 3000
+const port = process.env.PORT
 const session = require('express-session')
 
 app.set('view engine', 'ejs');
