05c9c4d5c7f62bb947b9846b7ccb7110dcf962f8
narinda giriputra <narinda.giriputra@gmail.com>
2022-01-27 10:06:06 +0800

    pullyangsama

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,6 @@
-
 const { User, Service, Detail } = require('../models')
+const bcrypt = require('bcryptjs');
+
 
 class Controller{
   static getUsers(req, res){
@@ -40,34 +41,32 @@ class Controller{
   }
 }
 
-module.exports = Controller
 
-const bcrypt = require('bcryptjs');
 
 class UserController{
   static getRegister(req, res){
     res.render('register')
   }
-
+  
   static postRegister(req, res){
     let {username, email, password, role} = req.body
     User.create({username, email, password, role})
-        .then(_=> res.redirect('/'))
-        .catch(err => {
-          if(err.name == 'SequelizeValidationError'){
-            let word = ''
-            word = err.errors.map(el => el.message)
-            res.redirect(`/register?${word.join(',')}`)
-          } else {
-            res.send(err)
-          }
-        })
+    .then(_=> res.redirect('/'))
+    .catch(err => {
+      if(err.name == 'SequelizeValidationError'){
+        let word = ''
+        word = err.errors.map(el => el.message)
+        res.redirect(`/register?${word.join(',')}`)
+      } else {
+        res.send(err)
+      }
+    })
   }
-
+  
   static getLogin(req, res){ 
     res.render('login')
   }
-
+  
   static postLogin(req, res){
     let {email, password} = req.body
     User.findOne({
@@ -90,8 +89,9 @@ class UserController{
         .catch(err => {
           res.send(err)
         })
-  }
+      }
 }
 
 module.exports = UserController
+module.exports = Controller
 
