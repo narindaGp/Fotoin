71245c5b8c6b0dabb82e5f1f9ffab11f25101c69
aryawdy <aryawdy16@gmail.com>
2022-01-27 23:16:07 +0700

    test

--- migrations/20220126131802-create-detail.js
+++ migrations/20220126131802-create-detail.js
@@ -15,7 +15,7 @@ module.exports = {
         type: Sequelize.TEXT
       },
       timeOfContract: {
-        type: Sequelize.STRING
+        type: Sequelize.INTEGER
       },
       createdAt: {
         allowNull: false,
